04d936c3d69c331ca6227efdce752aaa0d3e4000
Soulsender <soulsender@tutanota.com>
2022-07-05 11:56:43 -0700

    fixed b64

--- cogs/b64.py
+++ cogs/b64.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 import base64
 
@@ -12,23 +11,27 @@ class b64(commands.Cog):
 
     @commands.command()
     async def b64(self, ctx, action, string):
-        if action == "encode" or "e":
+        # "encode" or "e" entered
+        if action == "encode" or action == "e":
             string = string.encode('ascii')
             b64_bytes = base64.b64encode(string)
             output = b64_bytes.decode('ascii')
             await ctx.send(output)
             return [output, True]
+
+        # "decode" or "d" entered
         elif action == "decode" or "d":
             string = string.encode('ascii')
             b64_bytes = base64.b64decode(string)
             output = b64_bytes.decode('ascii')
             await ctx.send(output)
             return [output, True]
-        else:
-            embed = discord.Embed(title="__base64 Command Menu__", color=0x2b2a2a)
-            embed.add_field(value="**decode** or **d** - decode base64 encoded text", inline=False)
-            embed.add_field(value="**d** or **e** - encode base64 encoded text", inline=False)
-            await ctx.send(embed=embed)
+
+            # # HELP MENU
+            # embed = discord.Embed(title="__base64 Command Menu__", color=0x2b2a2a)
+            # embed.add_field(value="**decode** or **d** - decode base64 encoded text", inline=False)
+            # embed.add_field(value="**encode** or **e** - base64 encode text", inline=False)
+            # await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(b64(client))
--- main.py
+++ main.py
@@ -29,15 +29,13 @@ async def on_ready():
 @client.command()
 async def sourcehelp(ctx):
   embed = discord.Embed(title="__Source Code__", color=0x4287f5)
-  embed.add_field(name="Source code available on github", value="https://github.com/Soulsender/scarabbot",inline=False)
+  embed.add_field(name="Source code available on github", value="https://github.com/Soulsender/Earth-Invader", inline=False)
   await ctx.send(embed=embed)
 
 @client.command()
 async def help(ctx):
   embed = discord.Embed(title="__Command Menu__", color=0x2b2a2a)
-  embed.add_field(name="Useful", value="$rollhelp - open dice roll menu",inline=False)
-  embed.add_field(name="Miscellanous", value='$insult - send randomly generated insult', inline=False)
-  embed.add_field(name="Menus", value='$rollhelp - dice roll menu \n $adminhelp - admin menu \n $sourcehelp - view the bot source code \n $help - send this help menu', inline=False)
+  embed.add_field(name="Cryptography", value="**b64** {encode/decode} {\"string\"} - *encodes or decodes base64 text*", inline=False)
   await ctx.send(embed=embed)
 
 # mimic
