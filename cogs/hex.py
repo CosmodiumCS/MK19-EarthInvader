797553eb0a30d7c01660bab25930841f372d4989
Soulsender <soulsender@tutanota.com>
2022-07-06 21:50:40 -0700

    added hex encoding/decoding

---
+++ cogs/hex.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# reverse cipher package for the the codex project
+# created by : C0SM0
+
+import discord
+from discord.ext import commands
+import base64
+
+class hex(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('hex Online')
+
+    @commands.command()
+    async def hex(self, ctx, action, text):
+
+        # encode hex
+        if action == "encode" or action == "e":
+            # encode to hex
+            output = text.encode("utf-8").hex()
+            await ctx.send(output)
+            return [output, True]
+
+        # decode hex
+        if action == "decode" or action == "d":
+            # decode to hex
+            output = bytes.fromhex(text).decode("utf-8")
+            await ctx.send(output)
+            return [output, True]
+
+def setup(client):
+    client.add_cog(hex(client))
