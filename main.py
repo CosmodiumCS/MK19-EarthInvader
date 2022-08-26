5df8e18c16fb9b5d83a62c0187993effa3ae751c
Soulsender <soulsender@tutanota.com>
2022-08-25 20:02:21 -0700

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix="*", activity = discord.Game(name="I'm in fucking alpha don't expect much"))
+client = commands.Bot(command_prefix="*", activity = discord.Game(name="I'm in fucking alpha don't expect much"), intents=intents)
 client.remove_command('help')
 
 
