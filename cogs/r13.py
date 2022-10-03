50e89f586bede45f8d013a2f416514f5ef61f408
Fyzz <80930166+FyzzLive@users.noreply.github.com>
2022-10-03 11:36:45 -0700

    Added Rot13 Back

---
+++ cogs/r13.py
@@ -0,0 +1,77 @@
+# r13 [encoding] package for the the Skeleton Key project
+# created by : Fyzz
+
+import nextcord
+from nextcord.ext import commands
+from main import guild_id
+
+# match file name with classname
+class r13(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"R13 - Loaded")
+
+    @nextcord.slash_command(description="Rot13 Encode / Decode", guild_ids=[guild_id])
+    async def r13(self, interaction: nextcord.Interaction, action,text):
+        message = ""
+        key = 13
+
+        # "encode" or "e" entered
+        if action == "encode" or action == 'e':
+            output = ''
+            exclude = "\n\t .?!,/\\<>|[]{}@#$%^&*()-_=+`~:;\"'0123456789"
+
+            # encryption process
+            if text and key:
+                for character in text:
+                    if character in exclude:
+                        output += character
+                    elif character.isupper():
+                        output += chr((ord(character) +
+                                      int(key) - 65) % 26 + 65)
+                    else:
+                        output += chr((ord(character) +
+                                      int(key) - 97) % 26 + 97)
+            message = f"**Encoded:**\n{output}"
+
+        # "decode" or "d" entered
+        if action == "decode" or action == 'd':
+            output = ''
+            exclude = "\n\t .?!,/\\<>|[]{}@#$%^&*()-_=+`~:;\"'0123456789"
+
+            # decryption process
+            if text and key:
+                for character in text:
+                    if character in exclude:
+                        output += character
+                    elif character.isupper():
+                        output += chr((ord(character) -
+                                      int(key) - 65) % 26 + 65)
+                    else:
+                        output += chr((ord(character) -
+                                      int(key) - 97) % 26 + 97)
+            message = f"**Decoded:**\n{output}"
+
+        await interaction.response.send_message(message)
+    # Handle Errors
+
+    @r13.error
+    async def on_command_error(self, interaction: nextcord.Interaction, error):
+        message = """
+**Syntax**
+> Usage - `/r13`  `<encode/decode>`  `<text>`
+
+**Examples:**
+> Shorthand: `/r13`  `e`  `some text to encode`
+> Longhand: `/r13`  `decode`  `fbzr grkg`
+"""
+        embed = nextcord.Embed(title="SYNTAX ERROR",
+                               color=0xFE060A, description=message)
+        await interaction.channel.send(embed=embed)
+
+
+def setup(client) -> None:
+    client.add_cog(r13(client))
