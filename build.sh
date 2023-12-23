a2cf32ddd83d8b44458482f39c069efba19a9983
Soulsender <soul@soulsender.me>
2023-12-22 17:55:18 -0800

    created docker build script because I'm lazy

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t earth-invader .
+docker tag earth-invader soulsender/earth-invader:latest
+docker push soulsender/earth-invader:latest     
--- cogs/help.py
+++ cogs/help.py
@@ -33,6 +33,9 @@ class help(commands.Cog):
 > `/mor`  `<encode/decode>`  `<text>` - Morse Code
 > `/r47`  `<encode/decode>`  `<text>` - Rot47
 
+Example:
+`/b64 e some text` where `e` is to encode and `some text` is to be encoded.
+
 **[+] Miscellaneous Functions:**
 > `/replace`  `o` `5` `Hello World` - Replaces "o" with "5" in "Hello World"
 
